9d9cd97680294fd81792fcc2c37a50cd3a2903d0
DannyGalvan <cgalvan29111999@gmail.com>
2021-10-03 22:41:27 -0600

    modificando el js

--- index.js
+++ index.js
@@ -3,17 +3,11 @@ boton.addEventListener("click",alerta);
 var datos = document.getElementById("datos");
 var nombre = document.getElementById("nombre");
 var apellido = document.getElementById("apellido");
-var boton1 = document.getElementById("comprar");
-boton1.addEventListener("click",alerta1);
+
 
 function alerta() 
 {
     alert("su informacion ha sido enviada, " + nombre.value + " " + apellido.value + " " + "su conferencia sera de " + datos.value)    
-}
-
-function alerta1()
-{
-   alertas.innerHTML += "Se te enviara un Correo de Confirmación";
 }
 
 $(function()
